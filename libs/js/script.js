8334b366d16530fcda25091680fe2b501e291efc
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-11-25 15:40:29 +0000

    Created function to load the list of staff from the database

--- libs/js/script.js
+++ libs/js/script.js
@@ -3,20 +3,25 @@
 
 // Used to make sure that the everything on the page is loaded
 $(document).ready(function () {
+  // Initially load all of the images in the list.
+  loadStaffImageList();
+
   $("#UploadImageForm").on("submit", function (e) {
     e.preventDefault();
 
     const formData = new FormData(this);
 
-    // Send the AJAX request
+    // POST
+    // Insert staff image
     $.ajax({
       url: "./libs/php/insertStaffImage.php", // Backend PHP file
       type: "POST",
       data: formData,
-      processData: false, // Prevent jQuery from processing the data
-      contentType: false, // Prevent jQuery from setting contentType
+      processData: false,
+      contentType: false,
       success: function (response) {
         console.log(response);
+        loadStaffImageList();
       },
       error: function (jqXHR, textStatus, errorThrown) {
         console.error(errorThrown);
@@ -24,3 +29,33 @@ $(document).ready(function () {
     });
   });
 });
+
+function loadStaffImageList() {
+  // Clear the existing list
+  $("#StaffImagesList").empty();
+
+  // Load the list from the database
+  $.ajax({
+    url: "./libs/php/getAllStaffImages.php", // Backend PHP file
+    type: "GET",
+    success: function (response) {
+      console.log(response);
+      response.data.forEach((staff) => {
+        $("#StaffImagesList").append(
+          `<li class="list-group-item">
+            ${staff.Staff_Name}
+            <span class="pull-right">
+              <a value="${staff.Id}" id="MoveUp"><img alt="Move up" class="icon" src="img/up.svg"></a>
+              <a value="${staff.Id}" id="MoveDown"><img alt="Close" class="icon" src="img/down.svg"></a>
+              <a value="${staff.Id}" id="Update"><img alt="Edit staff member" class="icon" src="img/edit.svg"></a>
+              <a value="${staff.Id}" id="Delete"><img alt="Remove staff member" class="icon" src="img/close.svg"></a>
+            </span>
+          </li>`
+        );
+      });
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error(errorThrown);
+    },
+  });
+}
---
+++ libs/php/deleteStaffImage.php
@@ -0,0 +1,11 @@
+<?php
+
+$executionStartTime = microtime(true);
+
+include("config.php");
+
+header('Content-Type: application/json; charset=UTF-8');
+
+
+
+?>
