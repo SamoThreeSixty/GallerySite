13ded40a629f9ee1f037cdcbfc1b97b787d17872
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-11-25 11:47:29 +0000

    Added starting files

---
+++ libs/php/admin.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>A&amp;P Gallery</title>
+<link href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12"> <img alt="A&amp;P" class="logo" src="img/ap.svg">
+      <label>Add a new image</label>
+      <div class="well">
+        <form action="/action_page.php">
+          <div class="form-group">
+            <label>Upload your image</label>
+            <div class="input-group"> <span class="input-group-btn"><span class="btn btn-default btn-file">Browse…
+              <input id="imgInp" type="file">
+              </span></span>
+              <input class="form-control" type="text">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="email">Staff name:</label>
+            <input class="form-control" id="email" type="email">
+          </div>
+          <button class="btn btn-default" type="submit">Upload</button>
+        </form>
+      </div>
+    </div>
+    <div class="col-lg-12">
+      <label>Edit/Re-order existing staff</label>
+      <ul class="list-group">
+        <li class="list-group-item">Francesca Hall <span class="pull-right"><a href="/"><img alt="Move up" class="icon" src="img/up.svg"></a><a href="/"><img alt="Move down" class="icon" src="img/down.svg"></a><a href="/"><img alt="Edit staff member" class="icon" src="img/edit.svg"></a><a href="/"><img alt="Remove staff member" class="icon" src="img/close.svg"></a></span></li>
+        <li class="list-group-item">Alison Blackledge<span class="pull-right"><a href="/"><img alt="Move up" class="icon" src="img/up.svg"></a><a href="/"><img alt="Move down" class="icon" src="img/down.svg"></a><a href="/"><img alt="Edit staff member" class="icon" src="img/edit.svg"></a><a href="/"><img alt="Remove staff member" class="icon" src="img/close.svg"></a></span></li>
+        <li class="list-group-item">Andy Taylor<span class="pull-right"><a href="/"><img alt="Move up" class="icon" src="img/up.svg"></a><a href="/"><img alt="Move down" class="icon" src="img/down.svg"></a><a href="/"><img alt="Edit staff member" class="icon" src="img/edit.svg"></a><a href="/"><img alt="Remove staff member" class="icon" src="img/close.svg"></a></span></li>
+        <li class="list-group-item">David Daglish<span class="pull-right"><a href="/"><img alt="Move up" class="icon" src="img/up.svg"></a><a href="/"><img alt="Close" class="icon" src="img/down.svg"></a><a href="/"><img alt="Edit staff member" class="icon" src="img/edit.svg"></a><a href="/"><img alt="Remove staff member" class="icon" src="img/close.svg"></a></span></li>
+        <li class="list-group-item">Michael Kane<span class="pull-right"><a href="/"><img alt="Move up" class="icon" src="img/up.svg"></a><a href="/"><img alt="Close" class="icon" src="img/down.svg"></a><a href="/"><img alt="Edit staff member" class="icon" src="img/edit.svg"></a><a href="/"><img alt="Remove staff member" class="icon" src="img/close.svg"></a></span></li>
+        <li class="list-group-item">Mike Bailey<span class="pull-right"><a href="/"><img alt="Move up" class="icon" src="img/up.svg"></a><a href="/"><img alt="Close" class="icon" src="img/down.svg"></a><a href="/"><img alt="Edit staff member" class="icon" src="img/edit.svg"></a><a href="/"><img alt="Remove staff member" class="icon" src="img/close.svg"></a></span></li>
+        <li class="list-group-item">Kulbinder Singh<span class="pull-right"><a href="/"><img alt="Move up" class="icon" src="img/up.svg"></a><a href="/"><img alt="Close" class="icon" src="img/down.svg"></a><a href="/"><img alt="Edit staff member" class="icon" src="img/edit.svg"></a><a href="/"><img alt="Remove staff member" class="icon" src="img/close.svg"></a></span></li>
+        <li class="list-group-item">John Hyland<span class="pull-right"><a href="/"><img alt="Move up" class="icon" src="img/up.svg"></a><a href="/"><img alt="Close" class="icon" src="img/down.svg"></a><a href="/"><img alt="Edit staff member" class="icon" src="img/edit.svg"></a><a href="/"><img alt="Remove staff member" class="icon" src="img/close.svg"></a></span></li>
+      </ul>
+    </div>
+    <div class="col-lg-12"> <a href="gallery.php" class="btn btn-default pull-right">View Gallery</a> </div>
+  </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> 
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ libs/php/gallery.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>A&amp;P Gallery</title>
+<link href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="profiles row"> <img alt="A&amp;P" class="logo" src="img/ap.svg">
+        <h1 class="text-center">The staff gallery</h1>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+          <div class="profile"> <img alt="Enter Alt Tag" class="img-responsive" src="img/fh.jpg">
+            <h5>Francesca Hall</h5>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+          <div class="profile"> <img alt="Enter Alt Tag" class="img-responsive" src="img/ab.jpg">
+            <h5>Alison Blackledge</h5>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+          <div class="profile"> <img alt="Enter Alt Tag" class="img-responsive" src="img/at.jpg">
+            <h5>Andy Taylor</h5>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+          <div class="profile"> <img alt="Enter Alt Tag" class="img-responsive" src="img/dd.jpg">
+            <h5>David Daglish</h5>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+          <div class="profile"> <img alt="Enter Alt Tag" class="img-responsive" src="img/mk.jpg">
+            <h5>Michael Kane</h5>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+          <div class="profile"> <img alt="Enter Alt Tag" class="img-responsive" src="img/mb.jpg">
+            <h5>Mike Bailey</h5>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+          <div class="profile"> <img alt="Enter Alt Tag" class="img-responsive" src="img/ks.jpg">
+            <h5>Kulbinder Singh</h5>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+          <div class="profile"> <img alt="Enter Alt Tag" class="img-responsive" src="img/jh.jpg">
+            <h5>John Hyland</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-12"> <a href="admin.php" class="btn btn-default pull-right">Edit/Add staff</a> </div>
+  </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script> 
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+</body>
+</html>
