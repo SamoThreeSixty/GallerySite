ddf53ebf4015657488a6b249d15ae2a1376f73fc
Sam Bradshaw <bradshaw.sa@yahoo.com>
2024-11-26 09:28:10 +0000

    Control on the function to display edit controls

--- libs/js/template.js
+++ libs/js/template.js
@@ -1,14 +1,25 @@
 // Here i will contain the template functions that can be used to generate the html on the page
 
-export function staffImagesTemplate(record) {
+export function staffImagesTemplate(record, edit = false) {
+  let controls;
+
+  if (edit) {
+    controls = `    
+    <a class="Confirm" id="Confirm_${record.Id}"><img alt="Confirm changes to staff record" class="icon" src="media/icons/check.svg"></a>
+    <a class="Cancel" id="Cancel_${record.Id}"><img alt="Cancel changes to staff record" class="icon" src="media/icons/close.svg"></a>`;
+  } else {
+    controls = `    
+    <a class="Update" id="Update_${record.Id}"><img alt="Edit staff member" class="icon" src="media/icons/edit.svg"></a>
+    <a class="Delete" id="Delete_${record.Id}"><img alt="Remove staff member" class="icon" src="media/icons/close.svg"></a>`;
+  }
+
   return `<li class="list-group-item d-flex justify-content-between">
     <input id="${record.Id}" value="${record.Staff_Name}" readonly></input>
   
     <span class="pull-right">
     <a class="MoveUp" id="MoveUp_${record.Id}"><img alt="Move up" class="icon" src="media/icons/up.svg"></a>
     <a class="MoveDown" id="MoveDown_ ${record.Id}"><img alt="Close" class="icon" src="media/icons/down.svg"></a>
-    <a class="Update" id="Update_${record.Id}"><img alt="Edit staff member" class="icon" src="media/icons/edit.svg"></a>
-    <a class="Delete" id="Delete_${record.Id}"><img alt="Remove staff member" class="icon" src="media/icons/close.svg"></a>
+    ${controls}
     </span>
   </li>`;
 }
